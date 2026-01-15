ba948d2814e7982df7308053b147d06f8a42da92
Lý Đại Cương (6510) <6510@ctmatsuyard.com>
2026-01-15 15:46:09 +0700

    chore: update sitemap and robots for custom domain

--- app/robots.ts
+++ app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://daicuong11.github.io/MyPortfolio/sitemap.xml",
+    sitemap: "https://lydaicuong.me/sitemap.xml",
   };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,11 +5,15 @@ export const dynamic = "force-static";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://daicuong11.github.io/MyPortfolio/en",
+      url: "https://lydaicuong.me/",
       lastModified: new Date(),
     },
     {
-      url: "https://daicuong11.github.io/MyPortfolio/vi",
+      url: "https://lydaicuong.me/en/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://lydaicuong.me/vi/",
       lastModified: new Date(),
     },
   ];
